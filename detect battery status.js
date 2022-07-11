8c7d5eede7afdc6651cc2da1cc30a836d825b168
Ishini Hathurusinghe <101358580+ishinihathurusinghe@users.noreply.github.com>
2022-07-11 10:48:37 +0530

    Update detect battery status.js

--- detect battery status.js
+++ detect battery status.js
@@ -3,7 +3,7 @@ const charge = document.getElementById("charge");
 const chargingTimeRef = document.getElementById("charging-time");
 
 window.onload = () => {
-  //For browsers that don't support the battery status API
+ 
   if (!navigator.getBattery) {
     alert("Battery Status Api Is Not Supported In Your Browser");
     return false;
